956685f9abe06c8bd057787349c3a92542a792fe
Lewiswanjau <lewiswanjau97@gmail.com>
2024-05-22 08:14:49 +0300

    introduced react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
-      <div>
-        <H1>Hello world</H1>
-      </div>
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://nweyxmbrzbvoixtyegfx.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53ZXl4bWJyemJ2b2l4dHllZ2Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyODA0MzMsImV4cCI6MjAzMTg1NjQzM30.f6mUR-wozim2YUjDs8As8vpISnkwHtn4g4uS7ISDhng";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
